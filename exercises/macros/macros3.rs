4e8c64c2b822c23b2bb9f52b213e04e4e1e4ca8d
Liang Jun <phualiangjun@gmail.com>
2021-11-07 21:01:13 +0800

    macros and thread

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -2,9 +2,10 @@
 // Make me compile, without taking the macro out of the module!
 // Execute `rustlings hint macros3` for hints :)
 
-// I AM NOT DONE
+// smlj.. https://doc.rust-lang.org/book/ch19-06-macros.html?highlight=macro_export#declarative-macros-with-macro_rules-for-general-metaprogramming
 
 mod macros {
+    #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
